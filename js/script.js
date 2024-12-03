2e16447bd829bf84aea85881f5fc04853aeaa817
Mst. Shayla Sharmin <shaylasharmin993@gmail.com>
2024-12-03 15:15:16 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+
+
+// jquery
+$(function(){
+
+
+  var mixer = mixitup('#mixit', {
+    
+    animation: {
+        duration: 300
+    }
+});
+
+ $('.custom').slick({
+
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1
+
+});
+
+});
+
